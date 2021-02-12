a005cf3748c49b8b2919a384bb7ecc51e90deec6
tmiklu <61796331+tmiklu@users.noreply.github.com>
2021-02-12 16:43:26 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+
+#a = input("Vyberte si rad: ")
+#b = input("Vyberte si sedacku: ")
+#c = int(a)
+#d = int(b)
+
+
+arr = [
+      #['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0], 
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      ]
+
+
+# players
+rock   = arr[7][7] = 'R'
+knight = arr[7][6] = 'K'
+bishop = arr[7][5] = 'B'
+
+while True:
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            rock
+            knight
+            bishop
+            arr[7][4] = 'K'
+            arr[7][3] = 'Q'
+            arr[7][2] = 'B'
+            arr[7][1] = 'K'
+            arr[7][0] = 'R'
+            
+            arr[0][7] = 'R'
+            arr[0][6] = 'K'
+            arr[0][5] = 'B'
+            arr[0][4] = 'K'
+            arr[0][3] = 'Q'
+            arr[0][2] = 'B'
+            arr[0][1] = 'K'
+            arr[0][0] = 'R'
+            
+            
+            print(arr[i][j], end=' ')
+        print()
+    input("Move: ")
